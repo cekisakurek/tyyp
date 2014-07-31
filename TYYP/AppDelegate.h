9226e680e65773cf852d03c4e14799683be8b507
cihan emre <cekisakurek@gmail.com>
2014-07-31 15:49:39 +0200

    Initial Commit

---
+++ TYYP/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TYYP
+//
+//  Created by Cihan Emre Kisakurek on 31/07/14.
+//  Copyright (c) 2014 Cihan Emre Kisakurek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
